d0d8e471f98c105f56e34b39056fcdba30448192
Yogthos <dmitri.sotnikov@gmail.com>
2014-12-21 02:23:14 -0500

    fixed error handling

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-utils "0.1.0"
+(defproject reagent-utils "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- src/reagent/validation.cljs
+++ src/reagent/validation.cljs
@@ -60,7 +60,7 @@
   (try
     (js/parseFloat v)
     true
-    (catch Exception e
+    (catch :default  e
       false)))
 
 
