d5197ee476d1680cdba31af038fc97db42e86361
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2023-07-10 12:34:04 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-utils "0.3.6"
+(defproject reagent-utils "0.3.7"
   :description "various utility functions for Reagent based projects"
   :url "https://github.com/reagent-project/reagent-utils"
   :license {:name "Eclipse Public License"
